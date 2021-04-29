30b2d162d425d4ce47721620c320363697ea289f
isys35 <isys35@mail.ru>
2021-04-29 11:11:26 +0300

    f

--- wild_discount/config.py
+++ wild_discount/config.py
@@ -1,5 +1,5 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BOT_TOKEN = '1628794018:AAE6tItbigKsi38TAcC58G9OBrzMZUfp20Y'
+BOT_TOKEN = '1628794018:AAHe5GvrB08JQX8HTqh7I209I8vj6GKdnWk'
 CHAT_NAME = '-1001451746116'
